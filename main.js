3d03d02628f0a0f89d67a2ea69b0bb838267a81d
m.pavliuk <7plynet@gmail.com>
2019-10-08 12:39:29 +0300

    sms validator task completed

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let s="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or words etc.";
 let k=12;
 
-
 function checkLength(elem){
     let result=elem.join("");
     result=result.split(" ");
@@ -18,7 +17,7 @@ function solution(S, K){
         if(S[i].length>K){
             return -1;
         }
-        else if (checkLength(elements)+S[i].length<K){
+        if (checkLength(elements)+S[i].length<=K){
             elements.push(S[i]);
             str=elements.join(" ");
             if(S[i+1]){
@@ -31,17 +30,17 @@ function solution(S, K){
                 message.push(str);
             }
         }
+        else{
+            message.push(S[i])
+        }
     }
 return message
 }
 
 
-let arr=solution(s, k);
-for(let i=0; i<arr.length;i++){
-    console.log(arr[i].length);
-}
+// let arr=solution(s, k);
+// for(let i=0; i<arr.length;i++){
+//     console.log(arr[i].length);
+// }
 
 console.log(solution(s, k));
-
-
-
